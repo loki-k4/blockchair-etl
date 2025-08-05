56a0cb6aa95583d2aada6b3db57354f246218d44
lokii <lokeswarkudumula@gmail.com>
2025-08-05 18:27:03 -0400

    Add new DDL SQL files for Bitcoin data tables and update daily_download_blockchair.sh for improved directory structure; implement logging and error handling in snowflake_resource_manager.py

--- sql/ddl/database/create_database.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE DATABASE BLOCKCHAIR 
-COMMENT='Database For Crypto. Contains tables and views for blockchain analytics, ETL processes, and reporting.';
---- This database is designed to store and manage data related to various cryptocurrencies.
---- It includes tables for transactions, blocks, addresses, and other blockchain-related entities.
---- The database is optimized for ETL processes and reporting, allowing for efficient data retrieval and analysis.
---- Ensure that the necessary permissions and roles are set up for users who will access this database.
---- Adjust the database settings based on your specific requirements and workload characteristics.
